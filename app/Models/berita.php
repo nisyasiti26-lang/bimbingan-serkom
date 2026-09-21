219dcca0410c45ab69a96d320b0027194d625328
nisyasiti26-lang <nisyasiti26@gmail.com>
2026-09-21 11:17:10 +0700

    ..

--- app/Models/berita.php
+++ app/Models/berita.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class berita extends Model
 {
     //
+    //protected $table = 'berita';
+    //protected $primaryKey = 'id_berita';
+    //protected $typeKey = 'string';
 }
